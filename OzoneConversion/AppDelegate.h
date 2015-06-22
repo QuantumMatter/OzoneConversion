b0a007d9f8fbd2790edb9da1fb698261068b92fa
Craig Williford <craigjwilliford@gmail.com>
2015-06-22 10:00:29 -0600

    Added Project

---
+++ OzoneConversion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OzoneConversion
+//
+//  Created by Mac on 6/22/15.
+//  Copyright (c) 2015 davidkopala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
